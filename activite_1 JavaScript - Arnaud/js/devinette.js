cf2b2d39a89b83f683f5260237a642792ba91009
Arnaud63000 <40628189+Arnaud63000@users.noreply.github.com>
2018-07-10 16:05:04 +0200

    Add files via upload

---
+++ activite_1 JavaScript - Arnaud/js/devinette.js
@@ -0,0 +1,50 @@
+/* 
+Activité : jeu de devinette
+*/
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette By Arnaud!");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+//console.log("(La solution est " + solution + ")");
+
+// TODO : complétez le programme
+for(i=0; i<7; i++)
+{
+	nombre = Number(prompt("Trouvez le nombre mystère avec au maximum 6 essais: "));
+
+	if(nombre !== solution)
+	{
+
+		if(nombre>solution)
+		{
+			alert(nombre + " est trop grand ! ");
+
+
+				if(i==6)
+				{
+					alert("Tu as perdu ! La solution était "+ solution + " !");
+				}
+		}
+		else if(nombre<solution)
+		{
+			alert(nombre + " est trop petit ! ");
+
+				if(i==6)
+				{
+					alert("Tu as perdu ! La solution était "+ solution + " !");
+				}
+		}
+	}
+	else if(nombre === solution)
+	{
+		alert("Félicitation ! La solution est bien "+ solution + " !");
+		alert("Tu as réussi en " + i + " essai(s)!");
+		break;
+	}
+}
